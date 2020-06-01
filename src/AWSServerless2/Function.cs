7f98c6187d8aff3ab704ee3fdcdfd90f60ef33cb
Diego Garber <tegustaminuevacuenta@hotmail.com>
2020-05-31 20:50:29 -0500

    new version with core 31 baby!

--- src/AWSServerless2/Function.cs
+++ src/AWSServerless2/Function.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
 namespace AWSServerless2
 {
     public class Functions
